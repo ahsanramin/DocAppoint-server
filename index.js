acf5c5c746b38eaf784cff4804ec3d5f90a0971e
ahsanramin <ahsanramin25@gmail.com>
2026-05-23 14:50:03 +0600

    my 2nd commit

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ const uri = process.env.MONGODB_URI;
 
 
 const JWKS = createRemoteJWKSet(
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+
+
+
+// ✅ Middleware
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+const logger = (req, res, next) => {
